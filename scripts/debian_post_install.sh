73ed2ab79e924471996d9f0895e3584abaff3c51
Eduardo Leones <eduleones@gmail.com>
2020-02-10 13:55:34 -0300

    Update debian_post_install.sh

--- scripts/debian_post_install.sh
+++ scripts/debian_post_install.sh
@@ -10,7 +10,7 @@ sudo apt -y update && sudo apt -y upgrade
 # Base
 sudo apt -y install nano wget make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+xz-utils tk-dev libffi-dev liblzma-dev python-openssl git apt-transport-https
 
 # Ufw (Firewall)
 sudo apt -y install ufw && sudo ufw enable
@@ -23,16 +23,13 @@ gstreamer1.0-plugins-bad ffmpeg sox twolame vorbis-tools lame faad mencoder
 # Fonts
 sudo apt -y install fonts-powerline fonts-roboto fonts-ubuntu
 
+# Utils
+sudo apt -y install terminator arc arj cabextract lhasa p7zip p7zip-full p7zip-rar rar unrar unace unzip xz-utils zip
+
 # Google Chrome
 cd /tmp && wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" -O chrome.deb && sudo dpkg -i chrome.deb && sudo apt -y install -f
 
 # Vscode
 cd /tmp && wget "https://go.microsoft.com/fwlink/?LinkID=760868" -O vscode.deb && sudo dpkg -i vscode.deb && sudo apt -y install -f
 
-# Snap
-sudo apt install snapd
 
-# Snap apps
-sudo snap install spotify
-sudo snap install postman
-sudo snap install slack --classic
